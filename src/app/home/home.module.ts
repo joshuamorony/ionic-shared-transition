9555d9dae4b386df340b83607e9b2de577004964
Joshua Morony <joshuamorony@Joshuas-iMac.local>
2019-02-25 14:14:55 +1030

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { DetailPage } from '../detail/detail.page';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, DetailPage],
+  entryComponents: [DetailPage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { DetailPage } from '../detail/detail.page';
+import { myFadeInAnimation } from '../animations/fade-in';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public cards = new Array(10);
+
+  constructor(private modalCtrl: ModalController){
+
+  }
+
+  launchDetail(ev){
+
+    this.modalCtrl.create({
+      component: DetailPage,
+      enterAnimation: myFadeInAnimation,
+      componentProps: {
+        'coords': {
+          x: ev.target.x,
+          y: ev.target.y
+        }
+      }
+    }).then((modal) => {
+      modal.present();
+    });
+
+  }
 
 }
